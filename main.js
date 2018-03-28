91dc79505472b05915130b744c158959b29d8442
Thomas-E-Johnson <thomas.e.johnson1@gmail.com>
2018-03-28 23:07:38 +0100

    fullscreen

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('.fullScreen');
 
 function togglePlay() {
   const method = video.paused
@@ -38,6 +39,35 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function enterFullScreen() {
+  if (player.webkitRequestFullscreen)
+    player.webkitRequestFullscreen();
+  else if (player.requestFullscreen) 
+    player.requestFullscreen();
+  else if (player.mozRequestFullScreen)
+    player.mozRequestFullScreen();
+  }
+
+function exitFullScreen() {
+  if (document.exitFullscreen)
+    document.exitFullscreen();
+  else if (document.mozCancelFullScreen)
+    document.mozCancelFullScreen();
+  else if (document.webkitExitFullscreen)
+    document.webkitExitFullscreen();
+  }
+
+function isFullScreen() {
+  return (document.fullScreenElement && document.fullScreenElement !== null) || document.mozFullScreen || document.webkitIsFullScreen;
+}
+
+function toggleFullScreen() {
+  if (isFullScreen())
+    exitFullScreen();
+  else
+    enterFullScreen(document.documentElement);
+  }
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
@@ -53,4 +83,5 @@ progress.addEventListener('click', scrub)
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
-console.log(mousedown);
+
+fullScreen.addEventListener('click', toggleFullScreen);
